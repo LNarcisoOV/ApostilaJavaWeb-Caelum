1883f866dd6cce2dcdaf82f39239d2670a72954a
Leonardo Narciso <lnarcisoov@gmail.com>
2017-04-20 16:12:34 -0300

    [INFRA] Adicionando classe para teste da fabrica de conexões com o banco de dados.

--- src/main/java/br/com/apostilaJavaWebCaelum/infra/ConnectionFactory.java
+++ src/main/java/br/com/apostilaJavaWebCaelum/infra/ConnectionFactory.java
@@ -7,9 +7,11 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost/fj21", "root", "");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/apostilajavawebcaelum", "root", "123456");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+        	System.out.println("ERRO AO TENTAR CONECTAR COM O BANCO DE DADOS: " + e.getMessage());
+        	e.printStackTrace();
+        	throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/br/com/apostilaJavaWebCaelum/infra/TestaConexoesBanco.java
+++ src/main/java/br/com/apostilaJavaWebCaelum/infra/TestaConexoesBanco.java
@@ -1,5 +1,17 @@
 package br.com.apostilaJavaWebCaelum.infra;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TestaConexoesBanco {
 
+	public static void main(String... args) throws SQLException{
+		System.out.println("--------- INICIO DA EXECUÇÃO DA CLASSE DE TESTE. ----------");
+		System.out.println("EXECUTANDO CLASSE DE TESTE PARA CONEXÃO COM BANCO DE DADOS.");
+		Connection connection = new ConnectionFactory().getConnection();
+		System.out.println("Conexão efetuada com sucesso!");
+		connection.close();
+		System.out.println("Fechando conexão.");
+		System.out.println("----------- FIM DA EXECUÇÃO DA CLASSE DE TESTE. -----------");
+	}
 }
