1094831283e34b1d0a8b091dbab2aad1069e965c
Leonardo Narciso <lnarcisoov@gmail.com>
2017-04-24 10:09:26 -0300

    [CONTATO] Criando método que lista todos os contatos e finalização da parte de JDBC da apostila.

--- src/main/java/br/com/apostilaJavaWebCaelum/dao/ContatoDao.java
+++ src/main/java/br/com/apostilaJavaWebCaelum/dao/ContatoDao.java
@@ -3,7 +3,11 @@ package br.com.apostilaJavaWebCaelum.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import br.com.apostilaJavaWebCaelum.infra.ConnectionFactory;
 import br.com.apostilaJavaWebCaelum.model.Contato;
@@ -35,6 +39,39 @@ public class ContatoDao {
 			System.out.println("Ocorreu um erro ao tentar inserir contato.");
 			System.out.println(e.getMessage());
 			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Contato> pesquisaTodosOsContatos() {
+		List<Contato> contatos = new ArrayList<Contato>();
+		PreparedStatement stmt;
+		try {
+			stmt = this.connection.prepareStatement("select * from tb_contatos");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				// criando o objeto Contato
+				Contato contato = new Contato();
+				contato.setId(rs.getLong("id"));
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+
+				// montando a data através do Calendar
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+
+				// adicionando o objeto à lista
+				contatos.add(contato);
+			}
+			rs.close();
+			stmt.close();
+			return contatos;
+		} catch (SQLException e) {
+			System.out.println("Ocorreu um erro ao pesquisar contato.");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
