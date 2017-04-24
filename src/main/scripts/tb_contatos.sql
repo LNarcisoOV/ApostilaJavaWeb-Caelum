1b0751929d935766a33c2b87db228a3e378d8dfd
Leonardo Narciso <lnarcisoov@gmail.com>
2017-04-24 08:38:17 -0300

    [GIT] Adicionando pasta de scripts de banco de dados e primeiros scripts para contatos e login.

---
+++ src/main/scripts/tb_contatos.sql
@@ -0,0 +1,8 @@
+create table tb_contatos (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255),
+  email VARCHAR(255),
+  endereco VARCHAR(255),
+  dataNascimento DATE,
+  primary key (id)
+);
---
+++ src/main/scripts/tb_login.sql
@@ -0,0 +1,6 @@
+create table tb_login (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  login VARCHAR(255),
+  senha VARCHAR(255),
+  primary key (id)
+);
