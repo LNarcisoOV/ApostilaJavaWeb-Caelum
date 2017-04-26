c787fe2232ba158d0644a6a73c976d634c9f9d37
Leonardo Narciso <lnarcisoov@gmail.com>
2017-04-26 14:45:05 -0300

    [FILTER] Renomeando classe.

---
+++ src/main/java/br/com/apostilaJavaWebCaelum/filter/GeralFilter.java
@@ -0,0 +1,27 @@
+package br.com.apostilaJavaWebCaelum.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class GeralFilter implements Filter{
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+	    chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+	}
+
+}
